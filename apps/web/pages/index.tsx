61d7941da8c55288a35f17b4bff96076c8b9797d
Duc Pham <ducpmm@gmail.com>
2023-05-13 00:26:56 -0400

    Updated

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,6 @@
 import { Button } from '@frontend/ui';
 
 import { View, Text, Pressable } from 'react-native';
-import { styled } from 'nativewind';
-
-const BaseView = styled(View);
-const BaseText = styled(Text);
 
 const HomePage = () => {
   return (
@@ -12,11 +8,11 @@ const HomePage = () => {
       <Button />
 
       <Pressable onPress={() => console.log('HERE')}>
-        <BaseView>
-          <BaseText className="bg-blue-700">Welcome to button!</BaseText>
+        <View>
+          <Text className="bg-blue-700">Welcome to button!</Text>
           <Text>Welcome to 2!</Text>
           <div className="bg-green-300">BBB</div>
-        </BaseView>
+        </View>
       </Pressable>
     </div>
   );
