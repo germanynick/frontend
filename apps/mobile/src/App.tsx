0393f679f9d2121d1fceebadc450a4852eea3aa7
Duc Pham <ducpmm@gmail.com>
2023-07-21 02:10:20 -0400

    Config mobile

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,11 +1,12 @@
-import { UserListContainer } from '@frontend/features/users';
 import React from 'react';
 import { NativeBaseProvider } from 'native-base';
 import { FunctionComponent } from 'react';
+import { theme } from '@frontend/core/styles';
+import { UserListContainer } from '@frontend/features/users';
 
 export const App: FunctionComponent = () => {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <UserListContainer />
     </NativeBaseProvider>
   );
