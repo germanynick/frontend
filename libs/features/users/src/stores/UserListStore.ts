de0949ce096611ef75614ea8881260d1eba7bdc2
Duc Pham <ducpmm@gmail.com>
2023-07-31 13:54:14 -0400

    Update User Store

--- libs/features/users/src/stores/UserListStore.ts
+++ libs/features/users/src/stores/UserListStore.ts
@@ -1,41 +1,70 @@
-import { observable, action, makeObservable, flow } from 'mobx';
+import { observable, action, makeObservable, computed, runInAction } from 'mobx';
 
 export class UserListStore {
   constructor() {
     makeObservable(this);
   }
 
-  @observable loading: boolean = false;
-  @observable items: any[] = [];
-  @observable keyword: string = '';
+  @observable private _loading: boolean = false;
+  @observable private _items: any[] = [];
+  @observable private _keyword: string = '';
 
-  @action
-  setItems = (items: any[]) => {
-    this.items = items;
-  };
-
-  @flow.bound
-  *handleKeywordChangeAsync(keyword: string) {
-    this.keyword = keyword;
-    yield this.handleLoadMoreItemsAsync();
+  @computed
+  get loading() {
+    return this._loading;
   }
 
-  @flow.bound
-  *handleLoadMoreItemsAsync() {
+  @computed
+  get items() {
+    return this._items;
+  }
+
+  @computed
+  get keyword() {
+    return this._keyword;
+  }
+
+  set items(value: any[]) {
+    runInAction(() => {
+      this._items = value;
+    });
+  }
+
+  set loading(value: boolean) {
+    runInAction(() => {
+      this._loading = value;
+    });
+  }
+
+  set keyword(value: string) {
+    runInAction(() => {
+      this._keyword = value;
+    });
+  }
+
+  @action
+  handleKeywordChangeAsync = async (keyword: string) => {
+    this.keyword = keyword;
+    await this.handleLoadMoreItemsAsync();
+  };
+
+  @action
+  handleLoadMoreItemsAsync = async () => {
     try {
       if (this.loading) {
         return;
       }
 
       this.loading = true;
-      yield new Promise((resolve) => setTimeout(() => resolve('AA'), 1000));
+      await new Promise((resolve) => setTimeout(() => resolve('AA'), 1000));
+
       this.items = [...this.items, ...Array(20).fill({})];
     } catch (error) {
       console.debug(error);
     } finally {
       this.loading = false;
     }
-  }
+  };
 }
 
 export const userListStore = new UserListStore();
