641efa37756c50f677846711eb9ca6ae6341c74d
Duc Pham <ducpmm@gmail.com>
2023-08-03 12:55:56 -0400

    Add global config

---
+++ libs/core/config/src/configs.prod.ts
@@ -0,0 +1,10 @@
+import { IConfig } from './interfaces';
+
+export const CONFIGS: IConfig = {
+  env: 'production',
+
+  graphql: {
+    endpointUrl: 'https://therapy-jobs-qa.opusasia.io/graphql',
+    endpointToken: 'a3123aBcDeF',
+  },
+};
