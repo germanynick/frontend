fc58e520f2f88819e6e4b45c3d3e551e2b80002b
Duc Pham <ducpmm@gmail.com>
2023-09-13 11:58:38 -0400

    Update Core/UI

--- libs/core/ui/src/ThemeProvider.tsx
+++ libs/core/ui/src/ThemeProvider.tsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-restricted-imports
 import { GluestackUIProvider } from '@gluestack-ui/themed';
-import React from 'react';
+import { FC, ReactNode } from 'react';
 import { theme } from './theme';
 
 export interface IThemeProvider {
-  children?: React.ReactNode;
+  children?: ReactNode;
 }
 
-export const ThemeProvider: React.FC<IThemeProvider> = ({ children }) => {
+export const ThemeProvider: FC<IThemeProvider> = ({ children }) => {
   return <GluestackUIProvider config={theme}>{children}</GluestackUIProvider>;
 };
