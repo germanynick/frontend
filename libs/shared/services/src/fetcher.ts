3184f82cd83093b512a353d09131278b6ecca9a9
Duc Pham <ducpmm@gmail.com>
2023-09-24 14:29:35 -0400

    Remove graphql-request dependency

--- libs/shared/services/src/fetcher.ts
+++ libs/shared/services/src/fetcher.ts
@@ -1,25 +1,48 @@
 import { QueryFunctionContext } from '@tanstack/react-query';
-import { GraphQLClient } from 'graphql-request';
+// import { GraphQLClient } from 'graphql-request';
 
-const client = new GraphQLClient(process.env['NX_APP_GRAPHQL_URL'] || '', {
-  headers: () => {
-    return {
-      GraphiQL_Authorization: process.env['NX_APP_GRAPHQL_TOKEN'] || '',
-      'AUTH-TOKEN': '',
-    };
-  },
-});
+// const client = new GraphQLClient(process.env['NX_APP_GRAPHQL_URL'] || '', {
+//   headers: () => {
+//     return {
+//       GraphiQL_Authorization: process.env['NX_APP_GRAPHQL_TOKEN'] || '',
+//       'AUTH-TOKEN': '',
+//     };
+//   },
+// });
 
 export const fetcher =
   <TData, TVariables extends { [key: string]: any }>(
-    document: string,
+    query: string,
     variables?: TVariables,
     requestHeaders?: RequestInit['headers'],
   ) =>
   (_context?: QueryFunctionContext) => {
-    return client.request<TData>({
-      document,
-      variables,
-      requestHeaders,
-    });
+    return async () => {
+      const res = await fetch(process.env['NX_APP_GRAPHQL_URL'] || '', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          ...requestHeaders,
+        },
+        body: JSON.stringify({
+          query,
+          variables,
+        }),
+      });
+
+      const json = await res.json();
+
+      if (json.errors) {
+        const { message } = json.errors[0] || {};
+        throw new Error(message || 'Fetcher: Error');
+      }
+
+      return json.data as TData;
+    };
+
+    // return client.request<TData>({
+    //   document,
+    //   variables,
+    //   requestHeaders,
+    // });
   };
